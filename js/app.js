74c681e1af3b097c67ab581b74389581fa36bad2
samwise41 <134550253+samwise41@users.noreply.github.com>
2026-01-20 09:58:22 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -174,7 +174,7 @@
                             this.updateGearResult();
                             break;
                         case 'zones':
-                            content.innerHTML = await renderZonesTab();
+                            content.innerHTML = renderZonesTab();
                             break;
                         case 'roadmap':
                             content.innerHTML = renderRoadmap(this.garminData, this.planMd);
